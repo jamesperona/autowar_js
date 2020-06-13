4fcf35a604fcc4d5b16d6164af2d5b9afc311cf4
James Perona <j@mesperona.com>
2020-06-13 10:18:48 -0500

    Updates

---
+++ precache-manifest.70517558833acb604b2031cdc499da32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2edb65d0925264e6faabf4c43bd519bf",
+    "url": "/autowar_js/index.html"
+  },
+  {
+    "revision": "837eb20bc76a32459df7",
+    "url": "/autowar_js/static/css/main.01a6b8f7.chunk.css"
+  },
+  {
+    "revision": "348bc55b85d04deefee8",
+    "url": "/autowar_js/static/js/2.9857ba26.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/autowar_js/static/js/2.9857ba26.chunk.js.LICENSE"
+  },
+  {
+    "revision": "837eb20bc76a32459df7",
+    "url": "/autowar_js/static/js/main.b82d01f5.chunk.js"
+  },
+  {
+    "revision": "a312c7ae56dee965a8dc",
+    "url": "/autowar_js/static/js/runtime-main.9b7748e9.js"
+  },
+  {
+    "revision": "8163ae6bbd2d4f0392039265dd7b5445",
+    "url": "/autowar_js/static/media/club.8163ae6b.png"
+  }
+]);
